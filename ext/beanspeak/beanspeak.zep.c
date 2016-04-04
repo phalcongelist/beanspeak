43f0307f2755f2f048ca2fd4527369d97505c342
Serghei Iakovlev <sadhooklay@gmail.com>
2016-04-05 00:02:57 +0300

    Fixed issue #1 [ci skip]

--- ext/beanspeak/beanspeak.zep.c
+++ ext/beanspeak/beanspeak.zep.c
@@ -28,13 +28,23 @@
  * beanstalkd up to the latest version 1.10 is supported.
  *
  * <code>
+ * // Normal init with default params
  * use Beanspeak\Beanspeak;
  *
- * $queue = new Beanspeak([
+ * $queue = new Beanspeak;
+ *
+ * // Initialization with custom params
+ * use Beanspeak\Connection;
+ * use Beanspeak\Dispatcher;
+ * use Beanspeak\Beanspeak;
+ *
+ * $connection = new Connection([
  *     'host'       => '127.0.0.1',
  *     'port'       => 11300,
  *     'persistent' => true,
  * ]);
+
+ $queue = new Beanspeak(new Dispatcher($connection));
  * </code>
  *
  * @link https://github.com/kr/beanstalkd/
