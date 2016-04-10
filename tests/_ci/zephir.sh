d327065400642e547d05bd22b9aba2dc10044016
Serghei Iakovlev <sadhooklay@gmail.com>
2016-04-10 16:29:51 +0300

    Fixed Zephir & PHP runners

--- tests/_ci/zephir.sh
+++ tests/_ci/zephir.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 #
-# Dockerized Zephir
+#  Beanspeak
+#
+#  Copyright (c) 2016 Phalcon Team (http://www.phalconphp.com)
+#
+#  This source file is subject to the New BSD License that is bundled
+#  with this package in the file LICENSE.txt.
+#
+#  If you did not receive a copy of the license and are unable to
+#  obtain it through the world-wide-web, please send an email
+#  to license@phalconphp.com so we can send you a copy immediately.
+#
+#  Authors: Serghei Iakovlev <serghei@phalconphp.com>
 
-docker run -it --rm --name=zephir-$1 -v $(pwd):/zephir phalconphp/zephir:$1 "$@"
+VERSION=$1
+shift
+
+docker run -it --rm --name=zephir-$1 -v $(pwd):/zephir phalconphp/zephir:$VERSION "$@"
