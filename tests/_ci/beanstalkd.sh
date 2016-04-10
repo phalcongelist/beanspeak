0b32f266479d298099f9ede2bfde457929afe0f8
Serghei Iakovlev <sadhooklay@gmail.com>
2016-04-11 00:00:21 +0300

    Setup beanstalkd-network

---
+++ tests/_ci/beanstalkd.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+#
+#  Beanspeak
+#
+#  Copyright (c) 2016 Phalcon Team (http://www.phalconphp.com)
+#
+#  This source file is subject to the New BSD License that is bundled
+#  with this package in the file LICENSE.txt.
+#
+#  If you did not receive a copy of the license and are unable to
+#  obtain it through the world-wide-web, please send an email
+#  to license@phalconphp.com so we can send you a copy immediately.
+#
+#  Authors: Serghei Iakovlev <serghei@phalconphp.com>
+
+docker run -d --net=beanstalk_nw --name=beanstalk_srv phalconphp/beanstalkd:1.10 sh -c "beanstalkd -l 0.0.0.0 -p 11300"
--- tests/_ci/build.sh
+++ tests/_ci/build.sh
@@ -13,10 +13,17 @@
 #
 #  Authors: Serghei Iakovlev <serghei@phalconphp.com>
 
+# First create network
+# docker network create --driver bridge beanstalk_nw
+
+# Then run beanstalkd
+# docker run -d --net=beanstalk_nw --name=beanstalk_srv phalconphp/beanstalkd:1.10 sh -c "beanstalkd -l 0.0.0.0 -p 11300"
+
+# Then run
 docker run -it --rm \
   --entrypoint /entrypoint.sh \
   --privileged=true \
-  --net=bridge \
+  --net=beanstalk_nw \
   --name=test-beanspeak-${PHP_VERSION} \
   -v ${TRAVIS_BUILD_DIR}/tests/_ci/entrypoint.sh:/entrypoint.sh \
   -v ${TRAVIS_BUILD_DIR}/vendor:/app/vendor \
