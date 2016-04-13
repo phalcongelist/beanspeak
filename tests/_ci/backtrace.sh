b61950549c71994619d7d3f391fcc25921a94623
Serghei Iakovlev <sadhooklay@gmail.com>
2016-04-13 22:52:15 +0300

    Renamed after_failure script

---
+++ tests/_ci/backtrace.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+#
+#  Beanspeak
+#
+#  Copyright (c) 2016 Phalcon Team (http://www.phalconphp.com)
+#
+#  This source file is subject to the New BSD License that is bundled
+#  with this package in the file LICENSE.txt.
+#
+#  If you did not receive a copy of the license and are unable to
+#  obtain it through the world-wide-web, please send an email
+#  to license@phalconphp.com so we can send you a copy immediately.
+#
+#  Authors: Serghei Iakovlev <serghei@phalconphp.com>
+
+shopt -s nullglob
+export LC_ALL=C
+
+for i in /tmp/beanspeak/core-*.*; do
+	if [ -f "$i" -a "$(file "$i" | grep -o 'core file')" ]; then
+		gdb -q $(which php) "$i" <<EOF
+set pagination 0
+backtrace full
+info registers
+x/16i \$pc
+thread apply all backtrace
+quit
+EOF
+	fi
+done
