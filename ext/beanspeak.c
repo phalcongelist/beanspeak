cc27b70fab9056e4801ac9e9a54784ba6d7cded9
Serghei Iakovlev <serghei@phalconphp.com>
2016-04-25 21:13:22 +0300

    Used latest Zephir

--- ext/beanspeak.c
+++ ext/beanspeak.c
@@ -7,10 +7,6 @@
 
 #include <php.h>
 
-#if PHP_VERSION_ID < 50500
-#include <locale.h>
-#endif
-
 #include "php_ext.h"
 #include "beanspeak.h"
 
@@ -39,37 +35,16 @@ PHP_INI_END()
 
 static PHP_MINIT_FUNCTION(beanspeak)
 {
-#if PHP_VERSION_ID < 50500
-	char* old_lc_all = setlocale(LC_ALL, NULL);
-	if (old_lc_all) {
-		size_t len = strlen(old_lc_all);
-		char *tmp  = calloc(len+1, 1);
-		if (UNEXPECTED(!tmp)) {
-			return FAILURE;
-		}
-
-		memcpy(tmp, old_lc_all, len);
-		old_lc_all = tmp;
-	}
-
-	setlocale(LC_ALL, "C");
-#endif
 	REGISTER_INI_ENTRIES();
 	ZEPHIR_INIT(Beanspeak_Client);
 	ZEPHIR_INIT(Beanspeak_Exception);
 	ZEPHIR_INIT(Beanspeak_Job);
-
-#if PHP_VERSION_ID < 50500
-	setlocale(LC_ALL, old_lc_all);
-	free(old_lc_all);
-#endif
 	return SUCCESS;
 }
 
 #ifndef ZEPHIR_RELEASE
 static PHP_MSHUTDOWN_FUNCTION(beanspeak)
 {
-
 	zephir_deinitialize_memory(TSRMLS_C);
 	UNREGISTER_INI_ENTRIES();
 	return SUCCESS;
@@ -111,8 +86,6 @@ static PHP_RINIT_FUNCTION(beanspeak)
 	beanspeak_globals_ptr = ZEPHIR_VGLOBAL;
 
 	php_zephir_init_globals(beanspeak_globals_ptr TSRMLS_CC);
-	//zephir_init_interned_strings(TSRMLS_C);
-
 	zephir_initialize_memory(beanspeak_globals_ptr TSRMLS_CC);
 
 
@@ -121,9 +94,7 @@ static PHP_RINIT_FUNCTION(beanspeak)
 
 static PHP_RSHUTDOWN_FUNCTION(beanspeak)
 {
-
 	
-
 	zephir_deinitialize_memory(TSRMLS_C);
 	return SUCCESS;
 }
